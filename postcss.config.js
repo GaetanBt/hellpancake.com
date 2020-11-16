65afb799c272afd076dc7c3b20528e3f140dc96b
Gaëtan Bonnot <gaetan.bonnot@orange.com>
2020-11-16 14:37:24 +0100

    feat(css): implement CSS cache busting

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,11 @@ module.exports = {
     require('autoprefixer'),
     require('cssnano')({
       preset: 'default'
+    }),
+    /* @see https://brycewray.com/posts/2020/11/using-postcss-cache-busting-eleventy/ */
+    require('postcss-hash')({
+      trim: 20,
+      manifest: './src/_data/manifest.json'
     })
   ]
 }
