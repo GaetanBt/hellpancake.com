f2a008c9997bad9bb9357e44d82751a118e4cfd0
GaetanBt <gaetan.bonnot@gmail.com>
2024-07-25 18:28:26 +0200

    feat(i18n): enable multi language

--- site/config/config.php
+++ site/config/config.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-  'environment' => 'production'
+  'environment' => 'production',
+  'languages' => true
 ];
---
+++ site/languages/fr.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'code' => 'fr',
+    'default' => false,
+    'direction' => 'ltr',
+    'locale' => [
+        'LC_ALL' => 'fr_FR'
+    ],
+    'name' => 'Français',
+    'translations' => [
+        'changelang.label' => 'Changement de langue',
+        'changelang.pageAvailableIn' => 'Cette page est également disponible en Français',
+        'changelang.pageNotAvailableIn' => 'Cette page n\'est pas disponible en Français, vous serez redirigé vers l\'accueil',
+        'skiplinks.label' => 'Liens d\'évitement',
+        'skiplinks.content' => 'Aller au contenu',
+        'sitemap' => 'Plan de site',
+        'navigation.label' => 'Menu principal',
+        'navigation.home.link.label' => '<span class="text-stack">Hell</span> <span class="text-primary">Pancake</span><span class="sr-only">, retour à l\'accueil</span>',
+        'navigation.home.link.title' => 'Hell Pancake, retour à l\'accueil',
+        'post.author' => 'auteur',
+        'post.publishedDate' => 'Publié le',
+        'post.lastUpdated' => 'Modifié le'
+    ],
+    'url' => NULL
+];
