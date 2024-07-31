14976027c1116a0cf646e0c5c8985b059d5803b2
GaetanBt <gaetan.bonnot@gmail.com>
2024-07-31 18:37:26 +0200

    feat(changelang): add a changelang component

--- site/languages/en.php
+++ site/languages/en.php
@@ -9,18 +9,18 @@ return [
     ],
     'name' => 'English',
     'translations' => [
-      'changelang.label' => 'Switch lang',
-      'changelang.pageAvailableIn' => 'This page is also available in English',
-      'changelang.pageNotAvailableIn' => 'This page is not available in English, you will be redirected to the home page',
-      'skiplinks.label' => 'Skip links',
-      'skiplinks.content' => 'Go to content',
-      'sitemap' => 'Sitemap',
-      'navigation.label' => 'Main menu',
-      'navigation.home.link.label' => '<span class="text-stack">Hell</span> <span class="text-primary">Pancake</span><span class="sr-only">, go to homepage</span>',
-      'navigation.home.link.title' => 'Hell Pancake, go to homepage',
-      'post.author' => 'author',
-      'post.publishedDate' => 'Published on',
-      'post.lastUpdated' => 'Last updated on'
+        'changelang.label' => 'Switch lang',
+        'changelang.pageAvailableIn' => 'This page is also available in English',
+        'changelang.pageNotAvailableIn' => 'This page is not available in English, you will be redirected to the home page',
+        'skiplinks.label' => 'Skip links',
+        'skiplinks.content' => 'Go to content',
+        'sitemap' => 'Sitemap',
+        'navigation.label' => 'Main menu',
+        'navigation.home.link.label' => '<span class="text-stack">Hell</span> <span class="text-primary">Pancake</span><span class="sr-only">, go to homepage</span>',
+        'navigation.home.link.title' => 'Hell Pancake, go to homepage',
+        'post.author' => 'author',
+        'post.publishedDate' => 'Published on',
+        'post.lastUpdated' => 'Last updated on'
     ],
     'url' => '/'
 ];
