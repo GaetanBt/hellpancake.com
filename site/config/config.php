5e39bb3a1dd8642cd7d5494efe1337d4b37a2259
GaetanBt <gaetan.bonnot@gmail.com>
2024-08-05 22:47:13 +0200

    feat(blog): add blog templates and blueprints

--- site/config/config.php
+++ site/config/config.php
@@ -1,6 +1,12 @@
 <?php
 
+use GaetanBt\Kirby\Utilities\Helpers;
+
 return [
   'environment' => 'production',
-  'languages' => true
+  'languages' => true,
+  'wearejust.twig.env.filters' => [
+    'localizeDateField' => fn($date) => Helpers::localizeDateField($date)
+  ],
+  'date.handler'  => 'intl'
 ];
