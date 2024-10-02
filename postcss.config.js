515af67326107dca63c0d51f870ba948808f4920
GaetanBt <gaetan.bonnot@gmail.com>
2024-10-02 19:01:00 +0200

    chore(css): update postcss dependencies and remove hash generation

--- postcss.config.js
+++ postcss.config.js
@@ -4,11 +4,6 @@ module.exports = {
     require('autoprefixer'),
     require('cssnano')({
       preset: 'default'
-    }),
-    /* @see https://brycewray.com/posts/2020/11/using-postcss-cache-busting-eleventy/ */
-    require('postcss-hash')({
-      trim: 20,
-      manifest: './src/_data/manifest.json'
     })
   ]
 }
